fa625a0a6b11412e211649d01826493e35eea7a9
DopeMax <amingccc@gmail.com>
2018-09-24 17:44:39 +0800

    初始化项目结构

---
+++ my-app/src/admin.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
+import {
+    Row,Col
+} from 'antd'
+
+
+export default class Admin extends React.Component {
+    render() {
+        return (
+           <Row className="container">
+               <Col span="3" className="nav-left"><NavLeft></NavLeft></Col>
+               <Col span="21" className="main">
+                <Header>Header</Header>
+                <Row className="content">content</Row>
+                <Footer>Footer</Footer>
+               </Col>
+           </Row>
+        );
+    }
+}
